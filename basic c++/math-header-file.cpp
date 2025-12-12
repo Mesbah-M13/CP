0ef765104572d8b443b87408440cf3ce4d1d75b2
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-12 22:43:52 +0600

    reaplce all cp header file by bits/stdc++.h

--- basic c++/math-header-file.cpp
+++ basic c++/math-header-file.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <math.h>
 
@@ -8,4 +9,16 @@ int main()
   cout << "Hello World" << "\n";
   cout
       << sqrt(16);
+}
+*/
+// replace all the header file using single header file for cp & that is #include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int x = 64;
+  cout << "Claculating square root of " << x << " is " << sqrt(x);
 }
