520e94c85d9b1e2e6c2a1965e6c79ffdedc037ed
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-13 07:56:08 +0600

    add cpp variables

---
+++ basic c++/1-variables.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  // char , int , float , double , bool
+  // long int , long long int
+
+  char c = 'b';
+  double b = 5.600888884545345335345435545646f;
+  bool v = 120;
+  cout << "b = " << b << " | " << "c= " << c << " | " << v << endl;
+}
