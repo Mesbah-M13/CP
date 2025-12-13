7db2d6ab6f0d63d812ca6bfc62513f2edc0d8e4f
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-13 08:09:57 +0600

    add arithmetic & relational operators  increment & decrement operator

---
+++ basic c++/2-operator.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int a = 20;
+  cout << a++ << endl;
+  cout << a << endl;
+  cout << ++a << endl;
+
+  cout << int('0'); // type casting
+}
+
+// aritmatic & relational operators in c++
