4c66a4afa8e03f2789acebc3be32292c627cbb03
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-14 22:15:30 +0600

    add new files

---
+++ basic_cpp/4-input.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  string myName;
+  cout << "Type your name" << endl;
+  getline(cin, myName);
+  cout << "I'm " << myName;
+  return 0;
+}
