2d7a0c89cae59a1fbf8a7b749a76daab361042fd
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-14 08:12:43 +0600

    type casting & character ascii values

---
+++ basic c++/3-char-n-typecast.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  /*
+  char c = 'a';
+  cout << (int)c; // char to int type casting
+*/
+  char x = 'a';
+  char y = 'c';
+  cout << y - x; // ans: 2 ; ascii values difference
+}
