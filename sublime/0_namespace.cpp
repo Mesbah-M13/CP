d37b410e2c8a24595f228cf3c8df37a13a9b9f3d
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-26 21:00:54 +0600

    create hashmap structure

---
+++ sublime/0_namespace.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+namespace name_01{
+	void print(){
+		cout<<"Himel\n";
+	}
+}
+
+
+namespace name_02{
+	void print(){
+		cout<<"Mesbah";
+	}
+}
+
+int main(){
+	name_01::print();
+	name_02::print();
+
+}
