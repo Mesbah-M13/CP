1b1145e80e6a34349a14a94d7594150afa5ecc1c
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-27 07:07:01 +0600

    lookup operation using .containsKey

--- java/HashMapUsage.java
+++ java/HashMapUsage.java
@@ -6,9 +6,18 @@ public class HashMapUsage {
 
 //insert values
         countryMap.put("Bangladesh", 20);
-        countryMap.put("Chaina", 180);
+        countryMap.put("China", 180);
         countryMap.put("Australia", 10);
 
         System.out.println(countryMap);
+        countryMap.put("China",200);
+        System.out.println(countryMap);
+
+//        search or lookup operation for any pair using map_name.containsKey
+        if(countryMap.containsKey("Bangladesh")){
+            System.out.println("Bangladesh is there");
+        }else{
+            System.out.println("Not included");
+        }
     }
 }
