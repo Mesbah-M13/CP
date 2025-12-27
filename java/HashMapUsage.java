cc940942e30a11ab3b567af6c1a740d272fa6a10
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-27 08:00:34 +0600

    iteration in hashMap using entrySet()

--- java/HashMapUsage.java
+++ java/HashMapUsage.java
@@ -14,7 +14,7 @@ public class HashMapUsage {
         System.out.println(countryMap);
 
         String country = "Bangladesh";
-
+/*
 //        search or lookup operation for any pair using map_name.containsKey()
         if(countryMap.containsKey("Bangladesh")){
             System.out.println("Key is in the map");
@@ -24,6 +24,21 @@ public class HashMapUsage {
         }
 //      to find out the value related to the key use get()
         System.out.println("Population of " + country + " is :" + countryMap.get("Bangladesh"));
-        System.out.println(countryMap.get("Iraq"));
+        System.out.println(countryMap.get("Iraq")); // null
+
+//        iteration in hashmap general idea --> for( variable_data_type value : collection ) ; collection = bunch of values
+
+        int[] arr = {34, 53, 2};
+                for ( int val : arr){
+                    System.out.print(val +" ");
+                }
+
+//        in hashmpap --> for( Map.Entry<String,Integer>e : Map.entrySet())
+//        iteration using entrySet() --> for( Map.Entry<String,Integer>e : Map.entrySet()) [ e = variable name ]
+*/
+        for( Map.Entry<String,Integer> d : countryMap.entrySet()) {
+            System.out.println(d.getKey() + " : " + d.getValue());
+//            System.out.println(d.getValue());
+        }
     }
 }
