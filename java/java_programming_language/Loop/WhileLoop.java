8fcab6eb5541be8c441305ecdc64f597ac674362
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-30 22:25:01 +0600

    working with while loop

---
+++ java/java_programming_language/Loop/WhileLoop.java
@@ -0,0 +1,21 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+//        int winningTime = 1;
+//        while(winningTime <= 3){
+//            System.out.println("Argentina");
+//            winningTime++;
+
+//        6 am morning, 6 pm evening
+        int time = 1;
+        while(time <= 6){
+            System.out.println("This is morning");
+            time++;
+        }
+
+        int time2=1;
+        while(time2 <=18){
+            System.out.println("This is evening time");
+            time2++;
+        }
+    }
+}
