21d05dcbd5270730e9d6360681f8539d2cd0065c
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-30 22:59:51 +0600

    print java array using Arrays,toString()

--- java/java_programming_language/array/ArrayBasic.java
+++ java/java_programming_language/array/ArrayBasic.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+
 public class ArrayBasic {
     public static void main(String[] args) {
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
         System.out.println(cars[0]);
+
+        int[] nums= {12,15,25,8,40};
+        System.out.println(Arrays.toString(nums));
 
     }
 }
