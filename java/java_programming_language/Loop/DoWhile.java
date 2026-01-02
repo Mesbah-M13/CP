b23df5386798f7baef1107eb75106c3fb3a27994
Mesbah-M13 <mesbah0010@gmail.com>
2026-01-02 08:40:10 +0600

    find the sum of n natural numbers

--- java/java_programming_language/Loop/DoWhile.java
+++ java/java_programming_language/Loop/DoWhile.java
@@ -1,9 +1,25 @@
+//public class DoWhile {
+//    public static void main(String[] args) {
+//        int num = 10;
+//        do{
+//            System.out.println(num);
+//            num++;
+//        }while (num<20);
+//    }
+//}
+
+// find the sum of n natural numbers
+
 public class DoWhile {
     public static void main(String[] args) {
-        int num = 10;
-        do{
-            System.out.println(num);
-            num++;
-        }while (num<20);
-    }
+
+        int n = 4;
+        int res = 0;
+
+        while (n >=0) {
+        res = res + n;
+        n--;
+        }
+        System.out.println(res);
+        }
 }
