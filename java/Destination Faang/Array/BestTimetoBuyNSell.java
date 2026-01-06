ae38e7da49acf6a5a3114fec205d55517207de05
Mesbah-M13 <mesbah0010@gmail.com>
2026-01-06 23:12:56 +0600

    add DSA questions BestTimeToBuyAndSellStock

---
+++ java/Destination Faang/Array/BestTimetoBuyNSell.java
@@ -0,0 +1,24 @@
+public class BestTimetoBuyNSell {
+
+    public static int maxProfit(int[] prices ){
+        int profit = 0;
+        int min = prices[0];
+
+        for (int i = 0; i < prices.length; i++) {
+          if(prices[i]< min){
+                min = prices[i];
+            }
+            profit = Math.max(profit,prices[i] - min);
+        }
+        return profit;
+
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+
+        int result = maxProfit(prices);
+        System.out.println("Max profit: "+ result);
+
+    }
+}
