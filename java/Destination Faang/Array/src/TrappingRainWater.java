c2306c897b75aa7bd0079ce6cc811b8a99bed5f2
Mesbah-M13 <mesbah0010@gmail.com>
2026-01-18 08:00:10 +0600

    Trapping Rain Water with modified solution

--- java/Destination Faang/Array/src/TrappingRainWater.java
+++ java/Destination Faang/Array/src/TrappingRainWater.java
@@ -9,36 +9,28 @@ public class TrappingRainWater {
         int left = 0;
         int right = height.length - 1;
 
-        int leftMax = 0;
-        int rightMax = 0;
-
-        int totalWater = 0;
+        int total = 0;
+        int leftMax = height[0];
+        int rightMax = height[right];
 
         while (left < right) {
 
             if (height[left] <= height[right]) {
+                leftMax = Math.max(leftMax,height[left]);
 
-                if (height[left] >= leftMax) {
-                    leftMax = height[left];
-                } else {
-                    totalWater += leftMax - height[left];
+                if (leftMax - height[left] > 0 ) {
+                    total = total + leftMax-height[left];
                 }
-
                 left++;
-
             } else {
-
-                if (height[right] >= rightMax) {
-                    rightMax = height[right];
-                } else {
-                    totalWater += rightMax - height[right];
+                rightMax = Math.max(rightMax,height[right]);
+               if(rightMax-height[right] > 0){
+                   total = total + rightMax-height[right];
                 }
-
                 right--;
             }
         }
-
-        return totalWater;
+        return total;
     }
 
     public static void main(String[] args) {
